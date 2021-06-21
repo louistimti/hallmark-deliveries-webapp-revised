2918308ce35a4852f1823d2a7094ea9451626dda
louistimti <louistimti1@gmail.com>
2021-06-21 16:44:36 +0100

    Carousel attempt failed

---
+++ scripts/carousel.js
@@ -0,0 +1,43 @@
+// console.log('Hey Paul');
+
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+const dotsNav = document.querySelector('.carousel-nav');
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + 'px';
+};
+
+slides.forEach(setSlidePosition);
+
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+
+
+dotsNav.addEventListener('click', (e) => {
+    const targetDot = e.target.closest('button');
+
+    console.log(targetDot);
+
+
+
+    const currentSlide = track.querySelector('.current-slide');
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    console.log(targetIndex);
+
+    moveToSlide(track, currentSlide, targetSlide);
+
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+})
